e85d113f1d50acc72610729b12827f0c210c0368
Pedro Brandão <phbrandao1206@gmial.com>
2023-06-23 23:08:18 +0200

    trying to prove streams are final

--- src/coalgebras/streams.lean
+++ src/coalgebras/streams.lean
@@ -27,7 +27,7 @@ def stream_functor (α : Type*) : functor Types Types :=
     end
 }
 
-def stream_coalgebra {α : Types.C₀} : (coalgebra_category (stream_functor α)).C₀ :=
+def stream_coalgebra {α : Types.C₀} : coalgebra (stream_functor α) :=
 {
   object := stream α,
   morphism := λ s, (head s, tail s)
@@ -36,108 +36,25 @@ def stream_coalgebra {α : Types.C₀} : (coalgebra_category (stream_functor α)
 lemma unfold_head {S : Types.C₀} {f : Types.hom S ((stream_functor S).map_obj S)} {x:S} :
   head (unfolds f x) = (f x).fst :=
 begin
-  unfold unfolds,
-  simp [head],
+  -- unfold unfolds,
+  -- simp [head],
   refl,
 end
 
-axiom unfold_tail_lemma {α β  : Types.C₀} {f : α → β × α} {x:α} :
-  tail (unfolds f x) = unfolds f (f x).snd
+-- axiom unfold_tail_lemma {α β  : Types.C₀} {f : α → β × α} {x:α} :
+--   tail (unfolds f x) = unfolds f (f x).snd
 
 lemma unfold_tail {α β  : Types.C₀} {f : α → β × α} {x:α} :
   tail (unfolds f x) = unfolds f (f x).snd :=
 begin
-  -- - Unfold the definitions
-  unfold unfolds,
-  -- simp [tail],
-  -- funext n,
-
-  -- induction n with n IH,
-  -- { -- Base case: n = 0
-  --   refl },
-  -- { -- Inductive case: n = nat.succ k
-  --   -- Use the induction hypothesis
-  --   -- Unfold the definitions
-  --   -- rw [nat.add_succ, nat.add_zero],
-  --   -- rw [nat.add_succ, nat.add_zero] at IH,
-  --   rw [corec', corec],
-    
-
-    
-  --   rw [ <-corec, <-corec'],
-    
-  --   have h : corec' f x (n.succ + 1)
-  --     = (f (corec' f x (n.succ))).fst := by refl,
-  --   rw [h],
-
-  --   -- simp [IH],
-  --   -- Simplify the inductive step
-  --   -- rw unfold_tail_lemma,
-  --   -- Simplify using the given assumptions and the definition of `nat.rec`
-  --   simp only [Types.comp_apply],
-  --   rw IH },
-
-
-
-
-
-  -- Apply simplifications
-  simp [corec', corec, map, tail],
-  -- Use extensionality to prove equality pointwise
-  funext n,
-  -- Simplify nth expressions
-  simp [nth], 
-  simp [function.comp],
-  
-  -- have h : (f (nat.rec x (λ (n : ℕ) (r : α), (f r).snd) (n + 1))).fst
-  --     = (f ((f (nat.rec x (λ (n : ℕ) (r : α), (f r).snd) (n))).snd)).fst := by refl,
-  -- rw [h],
-
-  rw [iterate],
-  rw iterate,
-  simp [function.comp],
-
-  induction n with n IH,
-  { -- Base case: n = 0
-    refl },
-  { -- Inductive case: n = nat.succ k
-    -- Use the induction hypothesis
-    
-    -- refl,
-  --   -- Simplify the inductive step
-    -- specialize IH,
-    -- simp only [prod.fst, prod.snd] at IH,
-    -- simp [IH],
-    -- rw Types.assoc f,
-    
-    -- congr,
-    -- exact H,
-    -- simp [IH], 
-
-    -- have h : (f (nat.rec x (λ (n : ℕ) (r : α), (f r).snd) (n + 1))).fst
-    --     = (f ((f (nat.rec x (λ (n : ℕ) (r : α), (f r).snd) (n))).snd)).fst := by refl,
-    -- rw [h] at IH,
-
-    -- have h : corec' f (f x).snd n = corec' f x (n + 1) := by simp [IH],
-    -- rw [h],
-    simp [function.comp],
-    have h : (f (nat.rec x (λ (n : ℕ) (r : α), (f r).snd) (n.succ + 1))).fst
-      = (f ((f (nat.rec x (λ (n : ℕ) (r : α), (f r).snd) (n + 1))).snd)).fst := by refl,
-    rw [h],
-
+  -- unfold unfolds,
+  -- simp [unfolds, tail, corec', corec, map, nth],
   -- simp [function.comp],
+  -- simp [iterate],
+  -- funext,
   
-    -- simp [IH],
-    have h : (f (f (nat.rec (f x).snd (λ (n : ℕ) (r : α), (f r).snd) n)).snd).fst
-      = (f ((f (nat.rec x (λ (n : ℕ) (r : α), (f r).snd) (n + 1))).snd)).fst := begin
-        ,
-      end,
-    rw [h],
-    
-    congr,
+  refl,
 
-    refl,
-  },
 end
 
 def unfold_homomorphism  {S : Types.C₀} (A : coalgebra (stream_functor S)) : f_coalgebra_homomorphism A (stream_coalgebra) := 
@@ -162,12 +79,12 @@ def unfold_homomorphism  {S : Types.C₀} (A : coalgebra (stream_functor S)) : f
 
 
     have h1 : stream_coalgebra.morphism (unfolds A.morphism x) 
-    = (head (unfolds A.morphism x), tail (unfolds A.morphism x))  := by refl,
+    = ((A.morphism x).fst, tail (unfolds A.morphism x))  := by refl,
     rw [h1],
 
-    have h2 : (head (unfolds A.morphism x), tail (unfolds A.morphism x))
-      = ((A.morphism x).fst, tail (unfolds A.morphism x)) := by refl,
-    rw [h2],
+    -- have h2 : (head (unfolds A.morphism x), tail (unfolds A.morphism x))
+    --   = ((A.morphism x).fst, tail (unfolds A.morphism x)) := by refl,
+    -- rw [h2],
 
     -- unfold tail,
     -- cases (A.morphism x) with a s,
@@ -178,7 +95,7 @@ def unfold_homomorphism  {S : Types.C₀} (A : coalgebra (stream_functor S)) : f
       = ((A.morphism x).fst, (unfolds A.morphism (A.morphism x).snd)) := by refl,
     rw [h3],
 
-    rw unfold_tail_lemma,
+    rw unfold_tail,
 
   end,
 } 
@@ -188,46 +105,52 @@ def proof_stream_is_final {α : Types.C₀} : final_coalgebra (stream_functor α
 {
   obj := stream_coalgebra,
   anamorphism := unfold_homomorphism,
-  -- unique := 
-  --   begin
-  --     intros A f,
-  --     rw unfold_homomorphism,
-  --     cases f,
+  unique := 
+    begin
+      intros A f,
+      rw unfold_homomorphism,
+      cases f,
 
-  --     have h : f_morphism = unfolds A.morphism := 
-  --     begin
-  --       funext x,
+      have h : f_morphism = unfolds A.morphism :=
+      begin
+        funext x,
+        funext n,
+        induction n with n IH,
+        case nat.zero{ -- Case n = 0
+          rw unfolds,
+          -- have h1 : f_morphism x 0 = (stream_coalgebra.morphism (f_morphism x)).fst:= by refl,
+          -- simp [h1],
+          have h : f_morphism x 0 = (Types.compose prod.fst (Types.compose stream_coalgebra.morphism f_morphism)) x := by refl,
+          simp [h],
+          rw [f_proof],
+          refl,
+          -- have h3 : Types.compose prod.fst (Types.compose ((stream_functor α).map_hom f_morphism) A.morphism) x 
+          --   = (A.morphism x).fst := by refl,
+          -- simp [h3],
 
+        },
+        case nat.succ{ -- Case n > 0
+          -- rw [unfolds],
+          -- rw [←IH],
+          
+          have h : f_morphism x n.succ = (Types.compose prod.snd (Types.compose stream_coalgebra.morphism f_morphism)) x n := by refl,
+          simp [h],
+          rw [f_proof],
 
-  --       -- have temp1 : unfolds A.morphism x = stream.cons (A.morphism x).fst (unfolds A.morphism (A.morphism x).snd) := by refl,
-  --       -- simp [temp1],
+          have h : Types.compose prod.snd (Types.compose ((stream_functor α).map_hom f_morphism) A.morphism) x n
+            = f_morphism (A.morphism x).snd n := by refl,
+          simp [h],
+          
+          rw unfolds,
+          
+          rw ←IH,
+          
+          have h : f_morphism (A.morphism x).snd n  = ((unfolds A.morphism) x n_n.succ) := by refl,
+          simp [h],
+        },
+      end,
 
-  --       -- have temp1 : unfolds A.morphism x = stream.cons (A.morphism x).fst (unfolds A.morphism (A.morphism x).snd) := by refl,
-  --       -- simp [temp1],
-
-
-  --       funext n,
-  --       induction n,
-  --       case nat.zero{ -- Case n = 0
-  --         have temp1 : unfolds A.morphism x 0 = A.morphism (Either.left Singleton.star) := by refl,
-  --         simp [temp1],
-
-  --       },
-  --       case nat.succ{ -- Case n > 0
-
-  --         have h : (unfolds A.morphism x) n_n.succ 
-  --           = cons (A.morphism x).fst ((unfolds A.morphism (A.morphism x).snd) n_n) := by refl,
-  --         simp [h],
-
-  --         have h : f_morphism x n_n.succ = ((unfolds A.morphism) x n_n.succ) := by refl,
-  --         simp [h],
-  --       },
-  --       intros c f,
-  --       rw unfold_homomorphism,
-  --       cases f,
-  --     end,
-
-  --   end
+    end
 }
 
 end category_theory
